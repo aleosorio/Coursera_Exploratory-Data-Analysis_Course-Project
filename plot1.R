5ac00e0f0d9db312ada1001df793164ec6c68a56
Alejandro Osorio <alejandro@osorio.pro>
2018-03-05 18:40:04 -0300

    Final optimized version, with group_by, summarize and the pipe

--- plot1.R
+++ plot1.R
@@ -14,21 +14,15 @@ library(tidyverse)
 setwd("C:/Users/lenovo/Documents/MAO/Aprendizaje/Data Science/Exploratory Data Analysis_Coursera/Course Project/Data")
 
 # READING RAW DATA FROM WORKING DIRECTORY IN PC
-sumdat <- readRDS("summarySCC_PM25.rds")
-sccdat <- readRDS("Source_Classification_Code.rds")
+sumdat <- readRDS("summarySCC_PM25.rds") # no extra data required for this problem
 
 # GENERATING DATASET
-        ## Converting SCC variable from factor to character
-        sccdat$SCC <- as.character(sccdat$SCC)
-
-        ## Joining sccdat with sumdat (key = "SCC") into unique dataset
-        unidat <- left_join(sumdat, sccdat, by = c("SCC", "SCC"))
-
-        ## Subsetting required dataset into meandot to plot
-        findat <- filter(unidat, unidat$year %in% c(1999, 2002, 2005, 2008)) # filtering by years
-        meandat <- select(findat, year, Emissions) # selecting required variables
-        meandot <- tapply(meandat$Emissions, meandat$year, mean) # vector w/yearly emissions' means
-        meandot <- data.frame(names = as.integer(row.names(meandot)), emissions = meandot) # dataframe ready to plot
+## Subsetting, grouping and summaryzing into final dataset (findat)
+        findat <- sumdat %>%
+                filter(.$year %in% c(1999, 2002, 2005, 2008)) %>% # required years
+                select(year, Emissions) %>% # selecting required variables
+                group_by(year) %>%
+                summarize(Emissions_mean = mean(Emissions))
 
 # PLOTTING AND SAVING INTO FILE
         ## Opening PNG device to create "plot1.png"
@@ -36,8 +30,8 @@ sccdat <- readRDS("Source_Classification_Code.rds")
 
         ## Plotting
         par(mar = c(4,4,2,1), mfcol = c(1, 1))
-        with(meandot, plot(names, emissions, pch = 19 , xlab = "Year", ylab = "Yearly Avg Emissions"))
-        with(meandot, abline(lm(emissions ~ names), lwd = 2)) # linear yearly emissions' means
+        with(findat, plot(year, Emissions_mean, pch = 19 , main = "National Yearly Avg. Emissions", xlab = "Year", ylab = "Avg PM2.5 Emissions"))
+        with(findat, abline(lm(Emissions_mean ~ year), lwd = 2)) # linear yearly emissions' means
 
         ## Closing PNG device
         dev.off()
